039693fd5006f7a8c1890326fcf1150ba07d5da9
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-12 15:43:04 +0300

    adding component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main className="flex justify-center gap-4 flex-col min-h-screen">
-      
+    <main>
+      <Navbar />
     </main>
   );
 }
 
 export default App;
+// className="flex justify-center gap-4 flex-col min-h-screen"
