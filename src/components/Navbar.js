572267e81c18fa5ba1d54c1af6e0ccfef4e7ae57
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-12 23:29:33 +0300

    changed styles for Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-        <img src={Logo} alt="Logo Image" style={{width: "50px"}} />
+        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
       </div>
 
       <div>
-        <ul>
+        <ul className='flex'>
           <li>Home</li>
           <li>About</li>
           <li>Skills</li>
@@ -19,9 +19,15 @@ const Navbar = () => {
         </ul>
       </div>
 
-      <div><FaBars /></div>
+      <div className='hidden'><FaBars /></div>
 
-      <ul></ul>
+      <ul className='hidden'>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Work</li>
+        <li>Contact</li>
+      </ul>
 
       <div></div>
     </div>
