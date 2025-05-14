35945eacdd5cec30d9b80a06d07e5af579474d34
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-14 23:08:08 +0300

    added states

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,8 @@ import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
+  
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
