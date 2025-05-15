2d7110a6a937384629eef7bc972ed954c78d68c7
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-15 23:09:11 +0300

    changed component Home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
-import { FaBars, FaTimes } from 'react-icons/fa'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
@@ -34,7 +36,15 @@ const Navbar = () => {
         <li className='py-6 text-4xl'>Contact</li>
       </ul>
 
-      <div className='hidden'></div>
+      <div className='fixed flex flex-col top-[35%] left-0'>
+        <ul>
+          <li>
+            <a href="/">
+              LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
