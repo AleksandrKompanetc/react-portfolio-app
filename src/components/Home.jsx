f9620835782497b017426b439f146ca0aac87a3b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-16 23:05:13 +0300

    changed Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,12 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 const Home = () => {
   return (
     <div name="home" className='w-full h-screen bg-[#0a192f]'>
-      <div className='max-w-[1000px] mx-auto mx-auto px-8 flex flex-col justify-center h-full'></div>
+      <div className='max-w-[1000px] mx-auto mx-auto px-8 flex flex-col justify-center h-full'>
+        <p>Hi, my name is</p>
+        <h1>Aleksandr Kompanetc</h1>
+        <h2>I'm a Full Stack Developer</h2>
+        <p>I'm a full-stack developer specializing in building exceptional digital experiances.</p>
+      </div>
     </div>
   )
 }
