c826f1e8cba5134e103c97e051d5004e402c85ea
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-16 23:35:13 +0300

    creating About component

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300 '>About</div>
+  )
+}
+
+export default About
