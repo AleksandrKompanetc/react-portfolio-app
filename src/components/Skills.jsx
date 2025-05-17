39b19be9918c2de1a65ae0fe8a8c1fafcae8a9a7
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-17 23:55:39 +0300

    changed Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 
+import HTML from '../assets/html.png'
+
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div name='skills' className='bg-[#0a192f]'>
+      <div>
+        <div>
+          <p>Experience</p>
+          <p>These are the technologies I've work with</p>
+        </div>
+
+        <div>
+          <div>
+            <img src={HTML} alt="HTML icon" />
+            <p>HTML</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
