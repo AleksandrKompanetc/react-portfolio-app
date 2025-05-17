d51313a7dc0709ff80e40b5a02a63ca5cdb4e3fc
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-17 23:59:42 +0300

    added changes to styles of Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ import HTML from '../assets/html.png'
 const Skills = () => {
   return (
     <div name='skills' className='bg-[#0a192f]'>
-      <div>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
           <p>Experience</p>
           <p>These are the technologies I've work with</p>
@@ -13,7 +13,7 @@ const Skills = () => {
 
         <div>
           <div>
-            <img src={HTML} alt="HTML icon" />
+            <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
             <p>HTML</p>
           </div>
         </div>
