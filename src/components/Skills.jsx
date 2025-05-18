d726e6c90bc6f72315ddd027c8c9045e9f8e0392
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-18 23:54:45 +0300

    changed Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,11 +4,11 @@ import HTML from '../assets/html.png'
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f]'>
+    <div name='skills' className='bg-[#0a192f] text-gray-300'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div>
-          <p>Experience</p>
-          <p>These are the technologies I've work with</p>
+          <p className='text-4xl font-bold inline border-b-4 border-pink-600'>Experience</p>
+          <p className='py-4'>These are the technologies I've work with</p>
         </div>
 
         <div>
