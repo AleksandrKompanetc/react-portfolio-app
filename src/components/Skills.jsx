3e69c588f8dc74c9ccf7e5ebedc09a9711437eb6
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-19 22:24:46 +0300

    adding images for skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 
 import HTML from '../assets/html.png'
+import CSS from '../assets/css.png'
+import JAVASCRIPT from '../assets/javascript.png'
+import REACT from '../assets/react.png'
+import GITHUB from '../assets/github.png'
+import NODE from '../assets/node.png'
+import MONGO from '../assets/mongo.png'
+import AWS from '../assets/aws.png'
+
 
 const Skills = () => {
   return (
@@ -15,6 +23,34 @@ const Skills = () => {
           <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
             <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
             <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={JAVASCRIPT} alt="HTML icon" />
+            <p className='my-4'>JAVASCRIPT</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={REACT} alt="HTML icon" />
+            <p className='my-4'>REACT</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={GITHUB} alt="HTML icon" />
+            <p className='my-4'>GITHUB</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={NODE} alt="HTML icon" />
+            <p className='my-4'>NODE</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={MONGO} alt="HTML icon" />
+            <p className='my-4'>MONGO DB</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={AWS} alt="HTML icon" />
+            <p className='my-4'>AWS</p>
           </div>
         </div>
       </div>
