7077c101b14e71b8a49db459c75da8aa159d5ab8
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-19 23:17:18 +0300

    created custom style

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,7 +15,7 @@ const Work = () => {
           style={{backgroundImage: `url(${WorkImg})`}} 
           className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
           <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            <div>
+            <div className='opacity-0 group-hover:opacity-100'>
               <span></span>
               <div>
                 <a href="/">
