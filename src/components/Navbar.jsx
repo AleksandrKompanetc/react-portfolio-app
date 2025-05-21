6a75e0ad1ca90e6afa190c6811f6544bcddc7ec9
AleksandrKompanetc <moneymacker7@gmail.com>
2025-05-21 23:44:04 +0300

    added links for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes, FaGithub, FaLinkedin, FaFacebook, FaLinkedinIn } from 'react-icons/fa'
-import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
+import { HiOutlineMail } from 'react-icons/hi'
+import { BsFillPersonLinesFill } from 'react-icons/bs'
 import Logo from '../assets/logo.png'
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   const [nav, setNav] = useState(false)
@@ -16,11 +17,31 @@ const Navbar = () => {
 
 
       <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link to='home' smooth='true' duration='500' >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth='true' duration='500' >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth='true' duration='500' >
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth='true' duration='500' >
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth='true' duration='500' >
+            Contact
+          </Link>
+        </li>
       </ul>
 
 
@@ -39,34 +60,34 @@ const Navbar = () => {
       <div className='hidden fixed lg:flex flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
-            <a 
+            <a
               href="/"
               className='w-full flex justify-between items-center text-gray-300'
-              >
+            >
               LinkedIn <FaLinkedinIn size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
-            <a 
+            <a
               href="/"
               className='w-full flex justify-between items-center text-gray-300'
-              >
+            >
               Github <FaGithub size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
-            <a 
+            <a
               href="/"
               className='w-full flex justify-between items-center text-gray-300'
-              >
+            >
               Email <HiOutlineMail size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
-            <a 
+            <a
               href="/"
               className='w-full flex justify-between items-center text-gray-300'
-              >
+            >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
